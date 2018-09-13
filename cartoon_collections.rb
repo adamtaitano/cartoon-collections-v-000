7885612e43b4dbdcf8b8d9d9d371f1b9d0740a01
Adam Taitano <adam.taitano@gmail.com>
2018-09-13 06:06:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,9 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)# code an argument here
-  array.detect do |call|
+  if array.detect do |call|
     call.length > 4
+    end
   end
 end
 
